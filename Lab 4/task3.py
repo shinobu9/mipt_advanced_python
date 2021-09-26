3e6c00042a8b7bf7922690f13f61ece0e15b8cb6
Eliza Kamenski <kamelis113@gmail.com>
2021-09-26 21:17:12 +0300

    task 3 ready

---
+++ Lab 4/task3.py
@@ -0,0 +1,19 @@
+import functools
+
+def swap(func):
+    @functools.wraps(func)
+    def swap_wrapper(*args, **kwargs):
+        args_reversed = []
+        for i in range(len(args)-1, -1, -1):
+            args_reversed.append(args[i])
+        func(*args_reversed, **kwargs)
+    return swap_wrapper
+
+@swap
+def div(x, y, show=False):
+    res = x / y
+    if show:
+        print(res)
+    return res
+
+div(2, 5, show=True)
