9f01cf3b226eb4c6b524bed20320086d0d80edbb
Eliza Kamenski <kamelis113@gmail.com>
2022-01-24 03:33:38 +0300

    regex initial

---
+++ Lab 12 regex/advanced_sub.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# Для каждого регулярного выражения, которое требуется написать,
+# указана строка, в которой нужно выполнить замену, а следом
+# после стрелки (--->) указан результат замены
+
+
+
+# я опять не поняла что от меня хочет последний тест
+#'dog dog rat rat' != 'dog dog rat rat '
+#'dog dog' != 'dog dog '
+
+
+
+# aAc   ---> a!A!c
+# aZc   ---> a!Z!c
+# aZZc  ---> a!Z!!Z!c
+# aBaCa ---> a!B!a!C!a
+REGEXP_1 = '([A-Z])'   # регулярное выражение
+REGEXP_1_REPL = r'!\1!' # выражение для строки замены
+
+# abc    ---> abc
+# abbc   ---> abc
+# azzzc  ---> azc
+# arrrrc ---> arc
+# xxxxxx ---> x
+REGEXP_2 = r'([a-z])\1+'
+REGEXP_2_REPL = r'\1'
+
+# this is text         ---> this is text
+# this is is text      ---> this *is* text
+# this is is is text   ---> this *is* text
+# this is text text    ---> this is *text*
+# this is is text text ---> this *is* *text*
+REGEXP_3 = r'\b([a-z]+)(\s\1)+'  
+REGEXP_3_REPL = r'*\1*'
+
+# one two three ---> two one three
+# dog cat wolf  ---> cat dog wolf
+# goose car rat ---> goose rat car
+REGEXP_4 = r'(\b[a-z]{3})\s([a-z]{3})' 
+REGEXP_4_REPL = r'\2 \1'
+
+# cat dog                     ---> cat dog
+# cat dog cat                 ---> cat dog cat
+# dog cat dog cat cat         ---> dog dog
+# dog cat dog rat rat cat cat ---> dog dog rat rat
+REGEXP_5 = r'(^dog)([a-z ]+)(dog)\s((rat\s)+)?([a-z ]+)' 
+REGEXP_5_REPL = r'\1 \3 \4'
---
+++ Lab 12 regex/simple_match.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+# Для каждого регулярного выражения, которое требуется написать,
+# указаны строки, соответствующие этому выражению (они отмечены знаком +),
+# а также строки, не соответствующие этому выражению (отмечены знаком -)
+
+# + a
+# + ab
+# - b
+# - ba
+REGEXP_1 = '^a'
+
+
+# + aab
+# + abb
+# + acb
+# - ab
+# - aabc
+REGEXP_2 = '^a[a-c]b$'
+
+# + sofia.mp3
+# + sofia.mp4
+# - sofia.mp7
+# - sofia.mp34
+REGEXP_3 = '[a-z]+\.mp[34]$'
+
+# + taverna
+# + versus
+# + vera
+# + zveri
+# - zver
+REGEXP_4 = '[a-z]*ver[a-z]+'
+
+# - a
+# - aa
+# + aaa
+# - aaaa
+# - b
+# - bb
+# + bbb
+# - bbbb
+# - ccc
+REGEXP_5 = '[ab]{3}$'
+
+# - Ok
+# - OkOk
+# + OkOkOk
+# - OkOkOkOk
+# - ab
+# - abab
+# + ababab
+# - abababab
+REGEXP_6 = '(Ok|ab){3}$'
+
+# - aaa
+# - aaa aaa
+# + aaa Aaa aaa
+# + aaa aaa Aaa
+# + Aaa aaa aaa
+# - A
+# - aaa A aaa
+REGEXP_7 = '.*Aaa'
+
+# + abc
+# + abc03
+# + a-b-c-3
+# + a.b.c.0
+# - Aabc
+# - abc1
+# - #abc
+REGEXP_8 = '^a[^1]+$'
+
